08275e3e73ae0514391f6f728c7c0d3a014acc55
alipereira19 <aliper1908@gmail.com>
2024-12-12 02:48:45 +0000

    Terminé el proyecto

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,52 +1,28 @@
 import React from "react";
 
-const Cards = () => {
+
+
+const Cards = (props) => {
+    const { image, title, description } = props.cards;
+
+
     return (
-        <div>
-            <div class="card-group">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
+
+        < div class="card-group" >
+            <div class="card">
+                <img src={image} class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">{title}</h5>
+                    <p class="card-text">{description}</p>
                 </div>
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
+                <div class="card-footer">
+                    <button className="btn btn-primary btn-lg" type="button">
+                        Find out more!
+                    </button>
                 </div>
             </div>
-        </div>
-    )
-}
+        </div >
+    );
+};
 
 export default Cards; 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,28 @@ import Footer from "./Footer";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-
+const cards = [
+	{
+		image: "https://picsum.photos/200/300",
+		title: "Card title 1",
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+	},
+	{
+		image: "https://picsum.photos/200/300",
+		title: "Card title 2",
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+	},
+	{
+		image: "https://picsum.photos/200/300",
+		title: "Card title 3",
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+	},
+	{
+		image: "https://picsum.photos/200/300",
+		title: "Card title 4",
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+	}
+];
 
 //create your first component
 const Home = () => {
@@ -15,7 +36,16 @@ const Home = () => {
 		<div className="text-center">
 			<Navbar />
 			<Jumbotron />
-			<Cards />
+			<div className="container">
+				<div className="row">
+					{cards.map((item, index) =>
+						<div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+							<Cards cards={item} />
+						</div>
+					)
+					}
+				</div>
+			</div>
 			<Footer />
 		</div>
 	)
